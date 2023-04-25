37b631da425feb2b5b86be1f3ce727df64cc03b2
Cloudy_Place <minhnnm1009@gmail.com>
2023-04-26 03:09:56 +0700

    Function comment

--- Bloggie/Bloggie.Web/Pages/Blog/Details.cshtml.cs
+++ Bloggie/Bloggie.Web/Pages/Blog/Details.cshtml.cs
@@ -1,5 +1,7 @@
 using Bloggie.Web.Models.Domain;
 using Bloggie.Web.Repositories;
+using CloudBlog.Models.Domain;
+using CloudBlog.Models.ViewModels;
 using CloudBlog.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -13,27 +15,39 @@ namespace CloudBlog.Pages.Blog
         private readonly IBlogPostLikeRepository blogPostLikeRepository;
         private readonly SignInManager<IdentityUser> signInManager;
         private readonly UserManager<IdentityUser> userManager;
+        private readonly IBlogPostCommentRepository blogPostCommentRepository;
 
         public BlogPost BlogPost { get; set; }
 
-        public int TotalLikes { get; set; } 
-        public bool Liked { get; set; } 
+        public int TotalLikes { get; set; }
+        public bool Liked { get; set; }
+
+        [BindProperty]
+        public Guid BlogPostId { get; set; }
+
+        [BindProperty]
+        public string CommentDescription { get; set; }
+
+        public List<BlogComment> Comments { get; set; }
 
         public DetailsModel(IBlogPostRepository blogPostRepository, IBlogPostLikeRepository blogPostLikeRepository,
-            SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+            SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager,
+            IBlogPostCommentRepository blogPostCommentRepository)
 
         {
             this.blogPostRepository = blogPostRepository;
             this.blogPostLikeRepository = blogPostLikeRepository;
             this.signInManager = signInManager;
             this.userManager = userManager;
+            this.blogPostCommentRepository = blogPostCommentRepository;
         }
         public async Task<IActionResult> OnGet(string urlHandle)
         {
             BlogPost = await blogPostRepository.GetAsync(urlHandle);
 
-            if(BlogPost != null)
+            if (BlogPost != null)
             {
+                BlogPostId = BlogPost.Id;
                 if (signInManager.IsSignedIn(User))
                 {
                     var likes = await blogPostLikeRepository.GetLikesForBlog(BlogPost.Id);
@@ -41,10 +55,51 @@ namespace CloudBlog.Pages.Blog
                     var userId = userManager.GetUserId(User);
 
                     Liked = likes.Any(x => x.UserId == Guid.Parse(userId));
+
+                    await GetComments();
                 }
-                TotalLikes =  await blogPostLikeRepository.GetTotalLikesForBlog(BlogPost.Id);
+                TotalLikes = await blogPostLikeRepository.GetTotalLikesForBlog(BlogPost.Id);
             }
             return Page();
+        }
+
+        public async Task<IActionResult> OnPost(string urlHandle)
+        {
+            if (signInManager.IsSignedIn(User) && !string.IsNullOrWhiteSpace(CommentDescription))
+            {
+                var userId = userManager.GetUserId(User);
+
+                var comment = new BlogPostComment()
+                {
+                    BlogPostId = BlogPostId,
+                    Description = CommentDescription,
+                    DateAdded = DateTime.Now,
+                    UserId = Guid.Parse(userManager.GetUserId(User))
+                };
+                await blogPostCommentRepository.AddAsync(comment);
+            }
+            return RedirectToPage("/Blog/Details", new { urlHandle = urlHandle});
+
+
+        }
+
+        public async Task GetComments()
+        {
+            var blogPostComments = await blogPostCommentRepository.GetAllAsync(BlogPost.Id);
+
+            var blogCommentsViewModel = new List<BlogComment>();
+
+            foreach(var blogPostComment in blogPostComments)
+            {
+                blogCommentsViewModel.Add(new BlogComment
+                {
+                    DateAdded = blogPostComment.DateAdded,
+                    Description = blogPostComment.Description,
+                    Username = (await userManager.FindByIdAsync(blogPostComment.UserId.ToString())).UserName
+                });
+            }
+
+            Comments = blogCommentsViewModel;
         }
     }
 }
---
+++ Bloggie/Bloggie.Web/Repositories/BlogPostCommentRepository.cs
@@ -0,0 +1,27 @@
+﻿using Bloggie.Web.Data;
+using CloudBlog.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CloudBlog.Repositories
+{
+    public class BlogPostCommentRepository : IBlogPostCommentRepository
+    {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public BlogPostCommentRepository(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+        public async Task<BlogPostComment> AddAsync(BlogPostComment blogPostComment)
+        {
+            await bloggieDbContext.BlogPostComment.AddAsync(blogPostComment);
+            await bloggieDbContext.SaveChangesAsync();
+            return blogPostComment;
+        }
+
+        public async Task<IEnumerable<BlogPostComment>> GetAllAsync(Guid blogPostId)
+        {
+            return await bloggieDbContext.BlogPostComment.Where(x => x.BlogPostId == blogPostId).ToListAsync();
+        }
+    }
+}
